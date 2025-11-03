8f45c1d2897dd91020efc7a5a2bef79b43470409
CristianJaeger1705 <146154761+CristianJaeger1705@users.noreply.github.com>
2025-11-03 17:52:29 -0600

    Initial commit

---
+++ static/js/custom.js
@@ -0,0 +1,7 @@
+// Custom JS for Proyecto-TPI
+document.addEventListener('DOMContentLoaded', function () {
+  try {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    tooltipTriggerList.forEach(function (el) { new bootstrap.Tooltip(el); });
+  } catch (e) {}
+});
