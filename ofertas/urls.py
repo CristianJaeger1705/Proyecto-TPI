7072f976840ea965cbe276d6c9da5db6d65dba2f
SM0 <skydevice704@gmail.com>
2025-11-15 22:50:54 -0600

    se agrego url para eliminar

--- ofertas/urls.py
+++ ofertas/urls.py
@@ -6,4 +6,5 @@ urlpatterns=[
     path('ofertas',views.ofertas_List,name='ofertas'),
     path('ofertas/agregar',views.agregar_Campos,name="agregar"),
     path('ofertas/editar/<int:id>',views.editar_Campo,name="editar"),
+    path('eliminar/<int:id>',views.eliminar,name="eliminar"),
 ]
