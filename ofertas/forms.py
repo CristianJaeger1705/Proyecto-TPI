6226d927475c7d5f28505216c70564bfb6d42d3c
SM0 <skydevice704@gmail.com>
2025-11-15 22:17:01 -0600

    Formulario actualizado

--- ofertas/forms.py
+++ ofertas/forms.py
@@ -5,3 +5,12 @@ class Ofertasform(forms.ModelForm):
     class Meta:
         model = OfertaLaboral
         fields='__all__'
+        widgets = {
+            'tipo_empleo': forms.Select(attrs={'class': 'form-control'}),
+            'estado': forms.Select(attrs={'class': 'form-control'}),
+            'empresa': forms.Select(attrs={'class': 'form-control'}),
+            'titulo': forms.TextInput(attrs={'class': 'form-control'}),
+            'descripcion': forms.Textarea(attrs={'class': 'form-control', 'rows': 4}),
+            'salario': forms.NumberInput(attrs={'class': 'form-control'}),
+            'ubicacion': forms.TextInput(attrs={'class': 'form-control'}),
+        }
