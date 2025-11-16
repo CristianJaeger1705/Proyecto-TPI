28ec2b7efe293a77b40a2c7b1f7663cb3165b79c
SM0 <skydevice704@gmail.com>
2025-11-15 21:05:50 -0600

    Archivo modificado con las funciones crud, falta todavia XD

--- ofertas/views.py
+++ ofertas/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .models import OfertaLaboral
+from ofertas.forms import Ofertasform
 
 # Create your views here.
+#Lista todos las vacantes disponibles
+ofert = OfertaLaboral.objects.all()
+def ofertas_List(request):
+    return render(request, 'ofertas/index.html',{'ofertas': ofert })
+
+#Funcion para agregar una nueva vacante
+def agregar_Campos(request):
+    formulario = Ofertasform(request.POST or None)
+    if formulario.is_valid():
+      formulario.save()
+      return redirect('ofertas')
+    return render(request, 'ofertas/create.html',{'formulario': formulario})
+
+
+#Funcion para editar un dato
+def editar_Campo(request,id):
+    oferta=OfertaLaboral.objects.get(id=id)
+    formulario=Ofertasform(request.POST or None,instance=oferta)
+    if formulario.is_valid() and request.POST:
+        formulario.save()
+        return redirect('ofertas')
+    return render(request, 'ofertas/edit.html',{'formulario':formulario})
