33f55bc9bc1bce86ff0b7328dcbbd0ffb8ec870f
SM0 <skydevice704@gmail.com>
2025-11-15 22:17:38 -0600

    Vistas modificadas

--- ofertas/views.py
+++ ofertas/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import redirect, render
 from .models import OfertaLaboral
 from ofertas.forms import Ofertasform
-
+from django.core.paginator import Paginator
 # Create your views here.
 #Lista todos las vacantes disponibles
-ofert = OfertaLaboral.objects.all()
+
 def ofertas_List(request):
-    return render(request, 'ofertas/index.html',{'ofertas': ofert })
+    ofert = OfertaLaboral.objects.all().order_by('-fecha_publicacion')
+    # Paginar - 10 registros por página
+    paginator = Paginator(ofert, 10)
+    # Obtener el número de página desde la URL
+    page_number = request.GET.get('page')
+    # Obtener la página actual
+    ofertas = paginator.get_page(page_number)
+    return render(request, 'ofertas/index.html',{'ofertas': ofertas })
 
 #Funcion para agregar una nueva vacante
 def agregar_Campos(request):
