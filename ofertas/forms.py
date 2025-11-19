5284fca056db11cb8c7a13531dcdc92f0a3a42af
SM0 <skydevice704@gmail.com>
2025-11-18 23:43:36 -0600

    se modifico salario para que use dos decimales y mayores a cero

--- ofertas/forms.py
+++ ofertas/forms.py
@@ -11,7 +11,11 @@ class Ofertasform(forms.ModelForm):
             'empresa': forms.Select(attrs={'class': 'form-control'}),
             'titulo': forms.TextInput(attrs={'class': 'form-control'}),
             'descripcion': forms.Textarea(attrs={'class': 'form-control', 'rows': 4}),
-            'salario': forms.NumberInput(attrs={'class': 'form-control'}),
+            'salario': forms.NumberInput(attrs={
+                'class': 'form-control',
+                'step': '0.01',
+                'min': '0'
+            }),
             'ubicacion': forms.TextInput(attrs={'class': 'form-control'}),
         }
     def __init__(self, *args, **kwargs):
