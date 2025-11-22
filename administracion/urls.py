cb6ee525c25d9314e7390af3cbc2c5572beb142d
Mario Lima Gonzalez <lg16008@ues.edu.sv>
2025-11-22 17:27:58 -0600

    modulo usuario en panel administrativo
    cambios para el modulo admin, respecto a usuarios

--- administracion/urls.py
+++ administracion/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path('empresas/', views.admin_empresas, name='admin_empresas'),
     path('ofertas/', views.admin_ofertas, name='admin_ofertas'),
     path('postulaciones/', views.admin_postulaciones, name='admin_postulaciones'),
+    path('usuarios/', views.gestionar_usuarios, name='gestionar_usuarios'),
+    path('usuarios/activar/<int:user_id>/', views.activar_usuario, name='activar_usuario'),
+    path('usuarios/desactivar/<int:user_id>/', views.desactivar_usuario, name='desactivar_usuario'),
+    path('usuarios/verificar/<int:user_id>/', views.verificar_usuario, name='verificar_usuario'),
 ]
--- administracion/views.py
+++ administracion/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
+from usuarios.models import Usuario
+from django.shortcuts import redirect
+from django.contrib import messages
 
 # Solo permite acceso a usuarios administradores
 def es_admin(user):
@@ -23,3 +26,33 @@ def admin_ofertas(request):
 @user_passes_test(es_admin)
 def admin_postulaciones(request):
     return render(request, 'admin_postulaciones.html')
+
+#trabajo para el area de ususarios 
+def gestionar_usuarios(request):
+    usuarios = Usuario.objects.all().order_by('-fecha_registro')
+    return render(request, 'administracion/usuarios.html', {
+        'usuarios': usuarios
+    })
+#activa al usuario
+def activar_usuario(request, user_id):
+    usuario = Usuario.objects.get(id=user_id)
+    usuario.is_active = True
+    usuario.save()
+    messages.success(request, "Usuario activado correctamente.")
+    return redirect('gestionar_usuarios')
+
+#desactiva al usuario
+def desactivar_usuario(request, user_id):
+    usuario = Usuario.objects.get(id=user_id)
+    usuario.is_active = False
+    usuario.save()
+    messages.error(request, "Usuario desactivado.")
+    return redirect('gestionar_usuarios')
+
+#verifica al usuario
+def verificar_usuario(request, user_id):
+    usuario = Usuario.objects.get(id=user_id)
+    usuario.verificado = True
+    usuario.save()
+    messages.success(request, "Usuario marcado como verificado.")
+    return redirect('gestionar_usuarios')
