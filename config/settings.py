148c1e61ed571b95e1c9add9fb5a4a38927f5d25
Mario Lima Gonzalez <lg16008@ues.edu.sv>
2025-11-22 17:58:49 -0600

    usuario modificado
    modificaciones a usuario

--- config/settings.py
+++ config/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'aplicaciones',
     'administracion',
 ]
-
+AUTH_USER_MODEL = 'usuarios.Usuario'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
