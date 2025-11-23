0b444f394d092134f9de2c09b7780fcdc0396aec
Quibylix <barraza.fredimanuel@gmai.com>
2025-11-22 22:53:11 -0600

    ocultar usuario y contraseña para email

--- config/settings.py
+++ config/settings.py
@@ -139,5 +139,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "laburosv@gmail.com"
-EMAIL_HOST_PASSWORD = "whfe iqpb zeao vqwv"
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
