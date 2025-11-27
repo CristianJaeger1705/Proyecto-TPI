1759c26f6d1cf87d89db711514261cbe7fae0795
CristianJaeger1705 <146154761+CristianJaeger1705@users.noreply.github.com>
2025-11-26 22:20:43 -0600

    configuracion del Host para poder desplegar

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,11 @@ DATABASE_PASSWORD = os.getenv('DB_PASSWORD', 'admin')
 DATABASE_HOST = os.getenv('DB_HOST', 'localhost')
 DATABASE_PORT = os.getenv('DB_PORT', '5432')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+     'proyecto-tpi-jtgf.onrender.com',
+    'localhost',
+    '127.0.0.1'
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
