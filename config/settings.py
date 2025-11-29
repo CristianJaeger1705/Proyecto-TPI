b8151f7bf45515cd8265215025175b8eb748f2fe
gt22004 <146778905+gt22004@users.noreply.github.com>
2025-11-28 19:49:17 -0600

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -162,7 +162,7 @@ LOGIN_URL = 'usuarios:login'
 LOGIN_REDIRECT_URL = 'usuarios:redirigir'
 LOGOUT_REDIRECT_URL = 'home'
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
