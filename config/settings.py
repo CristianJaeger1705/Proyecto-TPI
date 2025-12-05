abcf705635ad060141255f2b0bde594f8cc5b90a
CristianJaeger1705 <146154761+CristianJaeger1705@users.noreply.github.com>
2025-12-04 19:05:12 -0600

    .

--- config/settings.py
+++ config/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
     'usuarios',
-    'usuarios.apps.UsuariosConfig',
+    #'usuarios.apps.UsuariosConfig',
     'ofertas',
     'perfiles',
     'postulaciones',
